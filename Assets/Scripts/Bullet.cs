6bedbed63f0d92902d0688ee267eef6e1108700c
Nguyễn Trường Tuệ <nguyentruongtue1996@gmail.com>
2022-06-22 13:55:45 +0700

    first commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+
+    public float timeToDestroy;
+
+    Rigidbody2D m_rb;
+
+    GameController m_gc;
+
+    AudioSource aus;
+
+    public AudioClip hitSound;
+
+    public GameObject hitVFX;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_gc = FindObjectOfType<GameController>();
+        aus = FindObjectOfType<AudioSource>();
+        Destroy(gameObject, timeToDestroy);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //thay doi position trong thanh phan transform
+        //dung phuong thuc AddForce cua thanh phan rigidbody2d
+        //Vector2.up = (0,1)
+        m_rb.velocity = Vector2.up * speed;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Enemy"))
+        {
+            //Tang diem so cua nguoi choi o day
+            m_gc.ScoreIncrement();
+
+            if(aus && hitSound)
+            {
+                aus.PlayOneShot(hitSound);
+            }
+
+            if (hitVFX)
+            {
+                Instantiate(hitVFX, col.transform.position, Quaternion.identity);
+
+                
+            }
+
+            
+
+            Destroy(gameObject);
+
+            Destroy(col.gameObject);
+
+            Debug.Log("Vien dan da va cham enemy");
+        }else if (CompareTag("SceneTopLimit"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject enemy;
+
+    public float spawnTime;
+
+    float m_spawnTime;
+
+    int m_score;
+
+    bool m_isGameover;
+
+    UIManager m_ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_spawnTime = 0;
+        m_ui = FindObjectOfType<UIManager>();
+        m_ui.SetScoreText("Score: " + m_score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_isGameover)
+        {
+            m_spawnTime = 0;
+            m_ui.ShowGameoverPanel(true);
+            return;
+        }
+
+        m_spawnTime -= Time.deltaTime;
+
+        if(m_spawnTime <= 0)
+        {
+            SpawnEnemy();
+
+            m_spawnTime = spawnTime;
+        }
+    }
+
+    public void SpawnEnemy()
+    {
+        float randXpos = Random.Range(-6f, 6f);
+
+        Vector2 spawnPos = new Vector2(randXpos, 7);
+
+        if(enemy)
+        {
+            Instantiate(enemy ,spawnPos, Quaternion.identity);
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void SetScore(int value)
+    {
+        m_score = value;
+    }
+
+    public int GetScore()
+    {
+        return m_score;
+    }
+
+    public void ScoreIncrement()
+    {
+        if (m_isGameover)
+        {
+            return;
+        }
+
+        m_score++;
+        m_ui.SetScoreText("Score: " + m_score);
+    }
+
+    public void SetGameoverState(bool state)
+    {
+        m_isGameover = state;
+    }
+
+    public bool IsGameover()
+    {
+        return m_isGameover;
+    }
+
+
+}
